bcae44392727a85596ed0d7be7b5707e5ec429a0
kumavis <aaron@kumavis.me>
2015-06-27 20:57:36 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Heroku defines the environment variable PORT, and requires the binding address to be 0.0.0.0
+var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
+var port = process.env.PORT || 9000;
+
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+  removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+  console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
